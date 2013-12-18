e1b99113829d56c9022bba2589c0b731c111a24b
John <JohnnyAD23@gmail.com>
2013-12-18 09:21:31 -0500

    Can move to other activities based on the spinner

--- TaxWizz/gen/com/Beta/taxwizz/R.java
+++ TaxWizz/gen/com/Beta/taxwizz/R.java
@@ -9,7 +9,12 @@ package com.Beta.taxwizz;
 
 public final class R {
     public static final class array {
-        public static final int Provinces=0x7f060000;
+        public static final int Provinces=0x7f070000;
+        public static final int Taxes=0x7f070001;
+        public static final int pref_example_list_titles=0x7f070002;
+        public static final int pref_example_list_values=0x7f070003;
+        public static final int pref_sync_frequency_titles=0x7f070004;
+        public static final int pref_sync_frequency_values=0x7f070005;
     }
     public static final class attr {
     }
@@ -20,35 +25,95 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int appicon=0x7f020000;
         public static final int categories=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int salestax=0x7f020003;
+        public static final int images=0x7f020003;
+        public static final int salestax=0x7f020004;
+        public static final int wallpaper=0x7f020005;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f090000;
-        public static final int action_settings=0x7f090004;
-        public static final int button1=0x7f090003;
-        public static final int spinner1=0x7f090001;
-        public static final int spinner2=0x7f090002;
+        public static final int LinearLayout1=0x7f0a0000;
+        public static final int action_settings=0x7f0a0016;
+        public static final int button1=0x7f0a0003;
+        public static final int checkBox1=0x7f0a000e;
+        public static final int checkBox2=0x7f0a000d;
+        public static final int checkBox3=0x7f0a000c;
+        public static final int checkBox4=0x7f0a000b;
+        public static final int editText1=0x7f0a0011;
+        public static final int editText2=0x7f0a0012;
+        public static final int editText3=0x7f0a0015;
+        public static final int editText4=0x7f0a0013;
+        public static final int editText5=0x7f0a0014;
+        public static final int spinner1=0x7f0a0001;
+        public static final int spinner2=0x7f0a0002;
+        public static final int spinner3=0x7f0a0007;
+        public static final int spinner4=0x7f0a000a;
+        public static final int textView1=0x7f0a000f;
+        public static final int textView2=0x7f0a0006;
+        public static final int textView3=0x7f0a0004;
+        public static final int textView4=0x7f0a0005;
+        public static final int textView5=0x7f0a0009;
+        public static final int textView6=0x7f0a0008;
+        public static final int textView7=0x7f0a0010;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int incometax=0x7f030001;
+        public static final int salestax=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int incometax=0x7f090000;
+        public static final int main=0x7f090001;
+        public static final int salestax=0x7f090002;
     }
     public static final class string {
-        public static final int SelectProvince=0x7f050004;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int ilikebigbuttonsandicannotlie=0x7f050003;
-        public static final int next=0x7f050005;
+        public static final int SelectProvince=0x7f060004;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int cal=0x7f06000d;
+        public static final int hello_world=0x7f060002;
+        public static final int ilikebigbuttonsandicannotlie=0x7f060003;
+        public static final int next=0x7f060005;
+        public static final int ontario=0x7f060008;
+        public static final int ontario1=0x7f060009;
+        public static final int pref_default_display_name=0x7f060019;
+        public static final int pref_description_social_recommendations=0x7f060017;
+        /**  Example settings for Data & Sync 
+         */
+        public static final int pref_header_data_sync=0x7f06001b;
+        /**  Strings related to Settings 
+ Example General settings 
+         */
+        public static final int pref_header_general=0x7f060015;
+        /**  Example settings for Notifications 
+         */
+        public static final int pref_header_notifications=0x7f06001e;
+        public static final int pref_ringtone_silent=0x7f060021;
+        public static final int pref_title_add_friends_to_messages=0x7f06001a;
+        public static final int pref_title_display_name=0x7f060018;
+        public static final int pref_title_new_message_notifications=0x7f06001f;
+        public static final int pref_title_ringtone=0x7f060020;
+        public static final int pref_title_social_recommendations=0x7f060016;
+        public static final int pref_title_sync_frequency=0x7f06001c;
+        public static final int pref_title_system_sync_settings=0x7f06001d;
+        public static final int pref_title_vibrate=0x7f060022;
+        public static final int province=0x7f06000a;
+        public static final int sales1=0x7f06000b;
+        public static final int sales2=0x7f06000c;
+        public static final int sales3=0x7f06000e;
+        public static final int sales4=0x7f06000f;
+        public static final int sales5=0x7f060010;
+        public static final int sales6=0x7f060011;
+        public static final int salesCalcu=0x7f060012;
+        public static final int select_tax=0x7f060006;
+        public static final int title_activity_incometax=0x7f060013;
+        public static final int title_activity_salestax=0x7f060007;
+        public static final int title_activity_settings=0x7f060014;
     }
     public static final class style {
         /** 
@@ -72,10 +137,16 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int pref_data_sync=0x7f040000;
+        public static final int pref_general=0x7f040001;
+        public static final int pref_headers=0x7f040002;
+        public static final int pref_notification=0x7f040003;
     }
 }
--- TaxWizz/src/com/Beta/taxwizz/MainActivity.java
+++ TaxWizz/src/com/Beta/taxwizz/MainActivity.java
@@ -2,7 +2,10 @@ package com.Beta.taxwizz;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +20,18 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void nextScreen (View clickedButton){
+		Spinner taxtype=(Spinner) findViewById(R.id.spinner2);
+		int position=taxtype.getSelectedItemPosition();
+		if(position==0){
+			Intent startActivity=new Intent(getApplicationContext(),SalestaxActivity.class);
+			startActivity(startActivity);
+		} else if (position ==1){
+			Intent startActivity=new Intent (getApplicationContext(),IncometaxActivity.class);
+			startActivity(startActivity);
+		}
 	}
 
 }
